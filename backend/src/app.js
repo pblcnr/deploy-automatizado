82834752fce343b09862be0c63dff71a0914cc63
Paulo Henrique de Andrade <plhnrqf@outlook.com>
2025-05-23 16:58:59 -0300

    Alteração do frontend para servir junto do backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,19 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import cors from "cors";
 import sequelize from "./models/sequelize.js";
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoutes);
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 const PORT = 3000;
 
