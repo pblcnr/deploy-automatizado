131510b2f535892556d1ccfb3a1fae103f497e77
Paulo Henrique de Andrade <plhnrqf@outlook.com>
2025-05-23 16:17:50 -0300

    Ajuste de portas para intervalo permitido no servidor

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const api = 'http://localhost:3000/users';
+  const api = 'http://localhost:8188/users';
 
   async function fetchUsers() {
     const res = await fetch(api);
